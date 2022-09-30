44dfdfabeab68380537f298eaaf3c0d2268dc411
Cr28self <tkhoon3study@gmail.com>
2022-09-30 13:43:16 +0900

    리덕스

--- src/index.js
+++ src/index.js
@@ -4,13 +4,20 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
 import App from './App';
+import {createStore} from "redux";
+import {devToolsEnhancer} from "@reduxjs/toolkit/src/devtoolsExtension";
+import {Provider} from "react-redux";
 
+
+const store = createStore(rootReducer,
+    devToolsEnhancer() //리덕스 devTool 적용
+    /*window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() */  ); //store 생성
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
+    <Provider store={store}>
         <BrowserRouter>
             <App/>
         </BrowserRouter>
-  
+    </Provider>
 );
---
+++ src/modules/login.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+const CHANGE_INPUT = 'login/CHANGE_INPUT'; //인풋값 변경
+
+
+
+
+const initialState = {
+    input: '',
+
+};
+//State의 초기 상태 정의
